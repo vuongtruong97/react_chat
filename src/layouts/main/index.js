bf40d6569996657756cc1dd737793151d4320348
vuong truong <truongquocvuongnb@gmail.com>
2022-12-12 17:22:28 +0700

    first commit

---
+++ src/layouts/main/index.js
@@ -0,0 +1,67 @@
+import { useLocation, Outlet } from 'react-router-dom';
+// @mui
+import { Box, Link, Container, Typography, Stack } from '@mui/material';
+// components
+import Logo from '../../components/Logo';
+//
+import MainFooter from './MainFooter';
+import MainHeader from './MainHeader';
+
+// ----------------------------------------------------------------------
+
+export default function MainLayout() {
+  const { pathname } = useLocation();
+
+  const isHome = pathname === '/';
+
+  return (
+    <Stack sx={{ minHeight: 1 }}>
+      <MainHeader />
+
+      <Outlet />
+
+      <Box sx={{ flexGrow: 1 }} />
+      <Box
+        sx={{
+          py: 5,
+          textAlign: 'center',
+          position: 'relative',
+          bgcolor: 'background.default',
+        }}
+      >
+        <Container>
+          <Logo sx={{ mb: 1, mx: 'auto' }} />
+
+          <Typography variant="caption" component="p">
+            © All rights reserved
+            <br /> made by &nbsp;
+            <Link href="https://snobdev.online/">snobdev.online</Link>
+          </Typography>
+        </Container>
+      </Box>
+
+      {/* {!isHome ? (
+        <MainFooter />
+      ) : (
+        <Box
+          sx={{
+            py: 5,
+            textAlign: 'center',
+            position: 'relative',
+            bgcolor: 'background.default',
+          }}
+        >
+          <Container>
+            <Logo sx={{ mb: 1, mx: 'auto' }} />
+
+            <Typography variant="caption" component="p">
+              © All rights reserved
+              <br /> made by &nbsp;
+              <Link href="https://snobdev.online/">snobdev.online</Link>
+            </Typography>
+          </Container>
+        </Box>
+      )} */}
+    </Stack>
+  );
+}
